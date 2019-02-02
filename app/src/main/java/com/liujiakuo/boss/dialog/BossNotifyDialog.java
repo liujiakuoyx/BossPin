d5c2420fc005cb3f19e5c6c066ee27586d0495d1
liujiakuoyx <627083017@qq.com>
2019-02-02 11:50:04 +0800

    删除无用代码

--- app/src/main/java/com/liujiakuo/boss/dialog/BossNotifyDialog.java
+++ app/src/main/java/com/liujiakuo/boss/dialog/BossNotifyDialog.java
@@ -1,12 +1,9 @@
 package com.liujiakuo.boss.dialog;
 
-import android.app.Dialog;
 import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.View;
 import android.widget.TextView;
 
