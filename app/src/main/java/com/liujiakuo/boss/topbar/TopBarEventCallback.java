41e9eae4a06404226f8932fc6eb93625ef0d2f12
liujiakuo <liujiakuo@corp.netease.com>
2019-02-25 15:39:40 +0800

    delete:好吧，放弃了

--- app/src/main/java/com/liujiakuo/boss/topbar/TopBarEventCallback.java
+++
@@ -1,9 +0,0 @@
-package com.liujiakuo.boss.topbar;
-
-/**
- * 顶栏事件回调，内部组件与顶栏根布局通信用
- */
-public interface TopBarEventCallback {
-
-    boolean handleEvent(int eventId, Object data);
-}
--- app/src/main/java/com/liujiakuo/boss/topbar/TopBarOp.java
+++
@@ -1,8 +0,0 @@
-package com.liujiakuo.boss.topbar;
-
-/**
- * liujiakuo on 2019/2/22 15:34
- */
-public interface TopBarOp<T> {
-    void op(T view);
-}
